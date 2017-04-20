4aa674661c39462236eb5eeff43e8c3b57c3b522
Prithvi Kambhampati <pkambham@mtu.edu>
2017-04-19 20:01:14 -0400

    Aswin: Added new navigation logic. More robust in open space

--- catkin_ws/src/egg_hunter/src/scripts/random_move.py
+++ catkin_ws/src/egg_hunter/src/scripts/random_move.py
@@ -3,6 +3,7 @@ import rospy
 import math
 import time
 import actionlib
+import random
 import tf
 import os
 import numpy
@@ -19,10 +20,17 @@ from geometry_msgs.msg import *
 class RandomMove(object):
     def __init__(self, timeout=None):
         rospy.init_node("RandomMove")
-
+        self.angular_min = 0
+        self.angular_max = 0
+        self.linear_min  = 0
+        self.linear_max  = 0
+        self.start_time  =  0
         self.listener = tf.TransformListener()
         self.saved_coord = []
+        self.runcount = 0
         self.count = 0
+        self.countLimit = random.randrange(25,75)
+        self.publish_msg = Twist()
 
         self.turnCoef = [(x ** 2 - 8100) / 10000000.0 for x in range(-90, 0)] + [(-x ** 2 + 8100) / 10000000.0 for x in range(0, 91)]
         self.speedCoef = [(-x ** 2 + 8100) / 10000000.0 for x in range(-90,91)]
@@ -62,20 +70,16 @@ class RandomMove(object):
         self.keyMsg = data.data
         print ("in key callback")
         rospy.loginfo("I heard key %s", data.data)
-ma
+
     def _latestScan(self, data):
         if (self.timeout and self.timeout <= time.time()) or self.keyMsg == 't':
             waypoints = rospy.get_param('waypoints')
 
             rospy.loginfo('Waypoints: %s', waypoints)
             rospy.loginfo('Dumping waypoints')
-<<<<<<< HEAD
-            os.system("rosparam dump ~/turbo-computing-machine/catkin_ws/src/egg_hunter/src/waypoints/waypoints.yaml /navigation/waypoints")h 
-=======
+            os.system("rosparam dump ~/turbo-computing-machine/catkin_ws/src/egg_hunter/src/waypoints/waypoints.yaml /navigation/waypoints")
 
             os.system("rosparam dump ~/EE5900_04/turbo-computing-machine/catkin_ws/src/egg_hunter/src/waypoints/waypoints.yaml /navigation/waypoints")
-
->>>>>>> 41b606b113f1e9afb0d1088bc276bc7fb2cc8588
             rospy.loginfo('Waypoints dumped')
 
             rospy.signal_shutdown("Execution timer expired")
@@ -115,60 +119,107 @@ ma
 
             self.keyMsg = ""
 
-        turnVal = 0
-        speedVal = 0
-        right_zone = data.ranges[0:65]
-        front_zone = data.ranges[65:115]
-        left_zone = data.ranges[115:180]
+        scale       =  1
+        self.angular_min = -1
+        self.linear_min  = -1
+        self.angular_max =  1
+        self.linear_max  =  1
+        self.start_time  =  0
 
-        front_zone_avg = sum(front_zone) / len(front_zone)
-        right_zone_avg = sum(right_zone) / len(right_zone)
-        left_zone_avg = sum(left_zone) / len(left_zone)
+        # Constants for laser averaging
+        front_delta = 15
+        side_ang    = 30
+        side_delta  = 15
+        side_thresh = 1.35
+        def toAng(rad):
+            ang = rad * 180 / 3.14
+            return ang
 
-        # If average is really REALLY close, might want to back up instead
-        #if front_zone_avg < 1.5 or min(front_zone) < 0.8:
-        if front_zone_avg < 4.5 or min(front_zone) < 1.8:
-            speedVal = 0.0
-            if self.last_turn > 0:
-                rospy.loginfo("Backing up to the left...")
-                turnVal = 1#0.5
-            else:
-                rospy.loginfo("Backing up to the right...")
-                turnVal = -1#-0.3
 
-        else:
-            rospy.loginfo("Normal hallway")
-            for p in range(0, 181):
-                # Inf range return means its over 10m from the LIDAR
-                if math.isinf(data.ranges[p]) or math.isnan(data.ranges[p]):
-                    speedVal = speedVal + (self.speedCoef[p] * 10)
-                    # Don't account long ranges into turn calcs
-                else:
-                    speedVal = speedVal + (self.speedCoef[p] * data.ranges[p])
+        # Averaged Sum of scan points function
+        def getSum(start, end, data):
+            angSum = float(0.0)
+            index = start
+            while index < end :
+                if data.ranges[index] < 15:
+                    angSum = angSum + data.ranges[index]
 
-                    # Turn away from walls
-                    turnVal = turnVal + (self.turnCoef[p] * data.ranges[p])
+                index = index + 1
 
-            #speedVal = min(speedVal * 1.2, 0.4) # sets max speed
-            speedVal = min(speedVal * 1.2, 1) # sets max speed
-            turnVal = turnVal * 1.4
+            angSum = float(angSum) / float(end-start)
 
-            if front_zone_avg < 3.0:
-                turnVal = turnVal * 2.0
-                speedVal = speedVal * 1.1
+            return angSum
 
-        cmd = Twist()
-        cmd.linear.x = speedVal
-        cmd.angular.z = turnVal
 
-        self.last_speed = speedVal
-        self.last_turn = turnVal
+        # Averaged Sum of scan points function
+        def getMin(start, end, data):
+            angSum = float(0.0)
+            index = start + 1
+            minScan = data.ranges[start]
+            while index < end :
+                if data.ranges[index] < minScan:
+                    prev = data.ranges[index]
 
-        rospy.loginfo(cmd)
+                index = index + 1
 
-        self.pub.publish(cmd)
+            return minScan
+        zeroAng    = int((((abs(data.angle_min) + abs(data.angle_max)) / data.angle_increment) / 2) - 1)
+        leftAng    = zeroAng + int(side_ang / toAng(data.angle_increment))
+        rightAng   = zeroAng - int(side_ang / toAng(data.angle_increment))
+        sideOffset = int(side_delta / toAng(data.angle_increment))
+        zeroOffset = int(front_delta / toAng(data.angle_increment))
 
-# standard ros boilerplate
+        # Compute averages for left, right, and front laser scan spans
+        leftAve  = getMin(leftAng, leftAng + sideOffset, data)
+        rightAve = getMin(rightAng - sideOffset, rightAng, data)
+        frontAve = getMin(zeroAng - zeroOffset, zeroAng + zeroOffset, data)
+
+        # Output for monitoring
+        #rospy.loginfo('\t%3.4f  -  %3.4f  -  %3.4f', leftAve, frontAve, rightAve)
+
+        # Set the threshold levels for randomization
+
+        # Too close in front, turn left and slowly back up
+        if frontAve < 1 :
+            self.angular_min = 0.25 * scale
+            self.angular_max = 0.5  * scale
+            self.linear_min  = -0.05 * scale
+            self.linear_max  = 0 * scale
+
+        # All Clear, randomly drive forward with varying turn
+        elif (frontAve > 2) and (leftAve > side_thresh) and (rightAve > side_thresh) :
+            self.angular_min = -1.25 * scale
+            self.angular_max = 1.25 * scale
+            self.linear_min  = 0.50 * scale
+            self.linear_max  = 1.0 * scale
+
+        # Close to a wall on one side, turn to side with most time
+        else :
+            if leftAve > rightAve :
+                self.angular_min = 0.75 * scale
+                self.angular_max = 1.0 * scale
+                self.linear_min  = 0.25 * scale
+                self.linear_max  = 0.75 * scale
+            else :
+                self.angular_min = -1.0 * scale
+                self.angular_max = -0.75 * scale
+                self.linear_min  = 0.25 * scale
+                self.linear_max  = 0.75 * scale
+                    # generate random movement mapping at random interval
+        if self.runcount < self.countLimit :
+                self.runcount = self.runcount + 1
+                self.pub.publish(self.publish_msg)
+        else :
+                self.runcount = 0
+                self.countLimit = random.randrange(5,25)
+                randLin = random.uniform(self.linear_min,self.linear_max)
+                randAng = random.uniform(self.angular_min,self.angular_max)
+                # push Twist msgs
+                linear_msg  = Vector3(x=randLin, y=float(0.0), z=float(0.0))
+                angular_msg = Vector3(x=float(0.0), y=float(0.0), z=randAng)
+                self.publish_msg = Twist(linear=linear_msg, angular=angular_msg)
+        rospy.loginfo('Published Twist')
+
 if __name__ == "__main__":
     try:
         run = RandomMove() # seconds
