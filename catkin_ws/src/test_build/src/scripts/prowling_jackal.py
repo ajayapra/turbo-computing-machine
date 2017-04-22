d08c80d9f580b604d9986bfcf372248acb33d4a7
RogerGomes29 <rwgomes@mtu.edu>
2017-04-22 14:54:14 -0400

    Made changes to run on Jackal

--- catkin_ws/src/test_build/src/scripts/prowling_jackal.py
+++ catkin_ws/src/test_build/src/scripts/prowling_jackal.py
@@ -18,7 +18,7 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist, Pose, PoseStamped, PoseWithCovarianceStamped, Vector3
 from move_base_msgs.msg	import MoveBaseGoal, MoveBaseAction
 from actionlib_msgs.msg import *
-# from ar_track_alvar_msgs.msg import AlvarMarkers
+#from ar_track_alvar_msgs.msg import AlvarMarkers
 
 import cv2
 import cv_bridge
@@ -46,8 +46,7 @@ def publish_markers():
         temp.action = Marker.ADD
         temp.type = Marker.ARROW
         temp.pose = wp.target_pose.pose
-        # temp.scale.x = 0.75
-        temp.scale.x = 0.65
+        temp.scale.x = 0.75
         temp.scale.y = 0.3
         temp.scale.z = 0.3
         temp.color.a = 1
@@ -66,16 +65,6 @@ class mapping(smach.State):
         self.angular_max = 0
         self.linear_min  = 0
         self.linear_max  = 0
-
-        # Max acceleration figure to smooth motion
-        self.linear_acc  =  0.01
-        self.angular_acc = 0.005
-        # Bounds on run counter random value
-        self.count_min = 25
-        self.count_max = 40
-        self.escape_command = 0
-        self.danger_flag = 0
-
         self.start_time  =  0
         self.halt = 0
         self.haltcount = 1
@@ -93,24 +82,32 @@ class mapping(smach.State):
         # Constants for laser averaging
         self.front_delta = 15
         self.side_ang    = 30
-        # self.side_delta  = 15
-        self.side_delta  = 20
-        # self.side_thresh = 1.35
-        self.side_thresh = 0.875
-        # self.scale = 1
+        #self.side_delta  = 15 
+        #self.side_thresh = 1.35
         self.scale = 0.65
         #self.keyMsg = ""
         self.timeout = None
-        self.ref_rate =50
+        self.ref_rate =100
         self.state_transition_flag = 0
-        self.randLin = float(0.0)
-        self.randAng = float(0.0)
-        self.linSet = float(0.0)
-        self.angSet = float(0.0)
         #Define timeout
         if self.timeout:
             self.timeout = time.time() + timeout
         #Publications and subscriptions
+        ###
+        self.linear_acc  =  0.01
+        self.angular_acc =  0.005
+        self.count_min = 25
+        self.count_max = 40
+        self.escape_command = 0
+        self.danger_flag = 0
+        self.side_delta  = 20
+        self.side_thresh = 0.875
+        self.randLin = float(0.0)
+        self.randAng = float(0.0)
+        self.linSet = float(0.0)
+        self.angSet = float(0.0)
+        ###
+    
         rospy.Subscriber("/front/scan", LaserScan, self._latestScan)
         rospy.Subscriber("/action_input", String, self.key_callback)
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
@@ -124,7 +121,7 @@ class mapping(smach.State):
 
     def _latestScan(self, data):
         def toAng(rad):
-            ang = rad * 180 / 3.14159
+            ang = rad * 180 / 3.14
             return ang
         # Averaged Sum of scan points function
         def getSum(start, end, data):
@@ -157,75 +154,88 @@ class mapping(smach.State):
         self.leftAve  = getMin(leftAng, leftAng + sideOffset, data)
         self.rightAve = getMin(rightAng - sideOffset, rightAng, data)
         self.frontAve = getMin(zeroAng - zeroOffset, zeroAng + zeroOffset, data)
+        rospy.loginfo('\t%3.4f  -  %3.4f  -  %3.4f', self.leftAve, self.frontAve, self.rightAve)
 
     def _move_bot(self):
         if self.frontAve < 1 :
-
-            self.linear_acc  =  1.0 * self.scale
-            self.angular_acc = 1.0 * self.scale
-
-            self.angular_min = 0.25 * self.scale
-            self.angular_max = 0.5 * self.scale
+            #self.angular_min = 0.25 * self.scale
+            self.angular_min = 0.3 * self.scale
+            self.angular_max = 0.5  * self.scale
             self.linear_min  = -0.05 * self.scale
             self.linear_max  = 0 * self.scale
+            ##
+            self.linear_acc  =  1.0 * self.scale
+            self.angular_acc =  1.0 * self.scale
             self.escape_command = 1
+            ##
 
         # All Clear, randomly drive forward with varying turn
         elif (self.frontAve > 1.75) and (self.leftAve > self.side_thresh) and (self.rightAve > self.side_thresh) :
-            self.linear_acc  =  0.00005 * self.scale
-            self.angular_acc = 0.00001 * self.scale
-
-            # self.angular_min = -1.25 * self.scale
-            # self.angular_max = 1.25 * self.scale
+            #self.angular_min = -1.25 * self.scale
+            #self.angular_max = 1.25 * self.scale
             self.angular_min = -0.625 * self.scale
             self.angular_max = 0.625 * self.scale
-
-            self.linear_min  = 0.50 * self.scale
-            self.linear_max  = 1.0 * self.scale
-
+        
+            #self.linear_min  = 0.50 * self.scale
+            #self.linear_max  = 1.0 * self.scale
+            
             self.linear_min  = 0.75 * self.scale
             self.linear_max  = 1.0 * self.scale
-
-            self.danger_flag=0
+            
+            self.linear_acc  =  0.0005 * self.scale
+            self.angular_acc =  0.0001 * self.scale
+    
+            self.danger_flag = 0
 
         # Close to a wall on one side, turn to side with most time
         else :
-            self.linear_acc  =  0.00005 * scale
-            self.angular_acc =  0.00001 * scale
+            self.linear_acc  =  0.0005 * self.scale
+            self.angular_acc =  0.0001 * self.scale
             self.escape_command = 1
 
             if self.leftAve > self.rightAve :
                 self.angular_min = 0.75 * self.scale
                 self.angular_max = 1.0 * self.scale
                 self.linear_min  = 0.25 * self.scale
-                self.linear_max  = 0.75 * self.scale
+                #self.linear_max  = 0.75 * self.scale
+                self.linear_max  = 0.50 * self.scale
             else :
                 self.angular_min = -1.0 * self.scale
                 self.angular_max = -0.75 * self.scale
-                # self.linear_min  = 0.25 * self.scale
-                # self.linear_max  = 0.75 * self.scale
                 self.linear_min  = 0.25 * self.scale
+                #self.linear_max  = 0.75 * self.scale
                 self.linear_max  = 0.50 * self.scale
                     # generate random movement mapping at random interval
-        if self.runcount < self.countLimit :
-             if (self.escape_command == 1) and (self.danger_flag == 0):
-                 self.danger_flag = 1
-                 self.escape_command = 0
-                 self.runcount = self.countLimit
-             else :
-                 self.runcount = self.runcount + 1
-                 self.pub.publish(self.publish_msg)
+        #if self.runcount < self.countLimit :
+        #        self.runcount = self.runcount + 1
+        #        self.pub.publish(self.publish_msg)
+        if self.runcount < self.countLimit:
+            if (self.escape_command == 1) and (self.danger_flag == 0):
+                self.danger_flag = 1
+                self.escape_command = 0
+                self.runcount = self.countLimit
+            else :
+                self.runcount = self.runcount + 1
+                
         else :
-            self.runcount = 0
-            self.countLimit = random.randrange(5,25)
-            self.randLin = random.uniform(self.linear_min,self.linear_max)
-            self.randAng = random.uniform(self.angular_min,self.angular_max)
-                 # Basic acceleration code
+                self.runcount = 0
+                #self.countLimit = random.randrange(5,25)
+                self.countLimit = random.randrange(self.count_min, self.count_max)
+                self.randLin = random.uniform(self.linear_min,self.linear_max)
+                self.randAng = random.uniform(self.angular_min,self.angular_max)
+                # push Twist msgs
+                #linear_msg  = Vector3(x=randLin, y=float(0.0), z=float(0.0))
+                #angular_msg = Vector3(x=float(0.0), y=float(0.0), z=randAng)
+
+        rospy.loginfo('randLin: %2f, randAng: %2f',self.randLin, self.randAng )
+
         if (self.randLin > self.linSet):
             if (self.randLin > (self.linSet + self.linear_acc)):
+                rospy.loginfo('1st if max acc exceeded')
                 self.linSet = self.linSet + self.linear_acc
             else :
                 self.linSet = self.randLin
+
         else :
             if (self.randLin < (self.linSet - self.linear_acc)):
                 self.linSet = self.linSet - self.linear_acc
@@ -242,20 +252,18 @@ class mapping(smach.State):
                 self.angSet = self.angSet - self.angular_acc
             else :
                 self.angSet = self.randAng
-                # push Twist msgs
-                rospy.loginfo('linSet:%2f',self.linSet)
-                rospy.loginfo('AngSet:%2f', self.angSet)
-                self.linear_msg  = Vector3(x=self.linSet, y=float(0.0), z=float(0.0))
-                self.angular_msg = Vector3(x=float(0.0), y=float(0.0), z=self.angSet)
+  
+        rospy.loginfo('LinSet:: %2f, AngSet:: %2f', self.linSet, self.angSet )     
 
-        self.publish_msg = Twist(linear=self.linear_msg, angular=self.angular_msg)
-
-        # if (self.halt == 1):
-		#     self.publish_msg = Twist()
-		# else:
-        #     self.publish_msg = Twist(linear=linear_msg, angular=angular_msg)
-	    #     publish_markers()
-
+        linear_msg  = Vector3(x=self.linSet, y=float(0.0), z=float(0.0))
+        angular_msg = Vector3(x=float(0.0), y=float(0.0), z=self.angSet)
+	if (self.halt == 1):
+	    self.publish_msg = Twist()
+	else :
+            self.publish_msg = Twist(linear=linear_msg, angular=angular_msg)
+        #self.publish_msg = Twist(linear=linear_msg, angular=angular_msg) 
+        self.pub.publish(self.publish_msg)
+	publish_markers()
         rospy.loginfo('Published Twist')
 
     def execute(self, userdata):
@@ -285,14 +293,14 @@ class get_waypoint(smach.State):
         self.pMap = PoseStamped()
         self.listener = tf.TransformListener()
 
-    # def marker_callback(self, data):
-    #     global alvar_num
-    #     alvar_num = data.markers[0].id
+    #def marker_callback(self, data):
+    #    global alvar_num
+    #    alvar_num = data.markers[0].id
 
     def execute(self, userdata):
         global temp_waypoint
         #define alvar subscribe and callback
-        # rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.marker_callback)
+        #rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self.marker_callback)
         self.pBase.header.frame_id = "/base_link";
         self.pBase.pose.position.x = 0.0;
         self.pBase.pose.position.y = 0.0;
@@ -343,8 +351,13 @@ class check_waypoint(smach.State):
             temp.target_pose.pose.orientation.w = temp_waypoint[3]
             waypoints.append(temp)
         #Turn robot 180 for a finite time
+	timeNow = time.time()
+        while (time.time() - timeNow < 2):
+	    linear_msg  = Vector3(x=float(-0.05), y=float(0.0), z=float(0.0))
+            angular_msg = Vector3(x=float(0.0), y=float(0.0), z=float(0.0))
+            self.pub.publish(Twist(linear=linear_msg, angular=angular_msg))	
         timeNow = time.time()
-        while (time.time() - timeNow < 20.8):
+        while (time.time() - timeNow < 15):
             linear_msg  = Vector3(x=float(0.0), y=float(0.0), z=float(0.0))
             angular_msg = Vector3(x=float(0.0), y=float(0.0), z=float(0.3))
             self.pub.publish(Twist(linear=linear_msg, angular=angular_msg))
