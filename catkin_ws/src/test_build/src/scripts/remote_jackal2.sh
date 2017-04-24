28feaae2f6196640a9c47f069a6169f582da9452
RogerGomes29 <rwgomes@mtu.edu>
2017-04-24 12:31:36 -0400

    Aswin: Debugging mapping

--- catkin_ws/src/test_build/src/scripts/remote_jackal2.sh
+++ catkin_ws/src/test_build/src/scripts/remote_jackal2.sh
@@ -1,2 +1,2 @@
-export ROS_MASTER_URI=http://jackal4:11311   # Hostname for Jackal 5
+export ROS_MASTER_URI=http://jackal2:11311   # Hostname for Jackal 2
 export ROS_IP="$(ifconfig | grep -A 1 'wlan0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
