65aabff7f661edf3482b516bbeb093c5d421fbeb
RogerGomes29 <rwgomes@mtu.edu>
2017-04-26 08:04:33 -0400

    Roger: Tweaking hunting.py

--- catkin_ws/src/test_build/src/scripts/hunting.py
+++ catkin_ws/src/test_build/src/scripts/hunting.py
@@ -24,66 +24,129 @@ import cv_bridge
 import argparse
 from sensor_msgs.msg import Image
 
-# class random_move(smach.State):
-#     def __init__(self):
-#         smach.State.__init__(self, outcomes=['outcome1'])
-#         self.angular_min = 0
-#         self.angular_max = 0
-#         self.linear_min  = 0
-#         self.linear_max  = 0
-#         self.start_time  =  0
-#         self.listener = tf.TransformListener()
-#         self.saved_coord = []
-#         self.runcount = 0
-#         self.count = 0
-#         self.countLimit = random.randrange(25,75)
-#         self.publish_msg = Twist()
-#         self.waypoints = []
-#         self.timeout = None
-#         self.turnCoef = [(x ** 2 - 8100) / 10000000.0 for x in range(-90, 0)] + [(-x ** 2 + 8100) / 10000000.0 for x in range(0, 91)]
-#         self.speedCoef = [(-x ** 2 + 8100) / 10000000.0 for x in range(-90,91)]
-#         self.last_speed = 0
-#         self.last_turn = 0
-#         self.keyTime = ""
-#         self.keyMsg = ""
-#
-#     def execute(self):
-#         rospy.Subscriber("/front/scan", LaserScan, self._latestScan)
-#         rospy.Subscriber("/lab_two_key", String, self.key_callback)
-#         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-#         self.viz_pub = rospy.Publisher("patrolling/viz_waypoints_array",
-#                                        MarkerArray, queue_size=10)
-#
-#         rospy.loginfo("Ready to get out there and avoid some walls!")
-#         rospy.logdebug(self.turnCoef)
-#         rospy.logdebug(self.speedCoef)
-#
-#         if timeout:
-#             self.timeout = time.time() + timeout
-#         rospy.spin()
-#
-# class get_waypoint(smach.State):
-#     def __init__(self):
-#         smach.State.__init__(self,outcomes=['got_waypoint'])
-#
-#     def execute(self, userdata):
-#         rospy.loginfo('getting waypoint')
-#         return 'got_waypoint'
-#
-# class check_waypoint(smach.State):
-#     def __init__(self):
-#         smach.State.__init__(self,outcomes=['waypoint_present','add_waypoint'])
-#
-#     def execute(self, userdata):
-#         rospy.loginfo('checking waypoint')
-#         # Subscribe to waypoint message and get the waypoint
-#         for i in len(waypoints):
-#             if got_waypoint in waypoints:
-#                 return 'waypoint_present'
-#             else:
-#                 return 'add_waypoint'
+#Navigate Waypoints State
+class prowl(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['prowl_pass'])
+        self.waypoints = []
+        self.waypoint_index = None
 
-def click_picture(smach.State):
+        rospy.init_node('waypoint_nav')
+        waypoint_arr = rospy.get_param('/navigation/waypoints')
+        rospy.loginfo('Printing arr:: %s', waypoint_arr)
+        rospy.loginfo('Length of array:: %2f',len(waypoint_arr)-1 )
+        for wp in waypoint_arr:
+            temp = MoveBaseGoal()
+
+            rospy.loginfo('wp[0]::%2f', wp[0]) #starting point of the course - navigate to it and detect alvar teg to set 
+            rospy.loginfo('wp[1]::%2f', wp[1])
+            rospy.loginfo('wp[2]::%2f', wp[2])
+            rospy.loginfo('wp[3]::%2f', wp[3])
+
+            temp.target_pose.header.frame_id = 'map'
+            temp.target_pose.pose.position.x = wp[0]
+            temp.target_pose.pose.position.y = wp[1]
+            temp.target_pose.pose.orientation.z = wp[2]
+            temp.target_pose.pose.orientation.w = wp[3]
+
+            self.waypoints.append(temp)
+
+
+        rospy.loginfo("Waypoint Nav ready.")
+
+    #Not needed, pop from the array each time
+    def _nearest_waypoint(self, pose):
+        if not self.waypoint_index:
+            shortest_len = float('inf')
+            shortest_len_index = 0
+            x = pose.pose.pose.position.x
+            y = pose.pose.pose.position.y
+
+            for i, wp in enumerate(self.waypoints):
+                test_len = math.hypot(wp.target_pose.pose.position.x - x,
+                                      wp.target_pose.pose.position.y - y)
+                if test_len < shortest_len:
+                    shortest_len = test_len
+                    shortest_len_index = i
+
+            self.waypoint_index = shortest_len_index
+
+    def _update_waypoints(self, data):
+        latest = MoveBaseGoal(target_pose = data)
+
+        if rospy.get_param('/waypoints_nav/patrolling/update_patrol'):
+            self.waypoints.insert(self.waypoint_index, latest)
+
+            if rospy.get_param('/waypoints_nav/patrolling/save_latest'):
+                ros.set_param_raw('/waypoints_nav/patrolling/waypoints',
+                                  self.waypoints)
+
+    def _publish_markers(self):
+        markers = []
+        for i, wp in enumerate(self.waypoints):
+            temp = Marker()
+
+            temp.header = wp.target_pose.header
+            temp.id = i
+            temp.ns = "patrolling"
+            temp.action = Marker.ADD
+
+            temp.type = Marker.ARROW
+            temp.pose = wp.target_pose.pose
+            temp.scale.x = 0.6
+            temp.scale.y = 0.3
+            temp.scale.z = 0.3
+            temp.color.a = 1
+            temp.color.r = 0
+            temp.color.g = 0.5
+            temp.color.b = 0.5
+            markers.append(temp)
+        self.viz_pub.publish(markers=markers)
+
+        rospy.loginfo("Markers Published")
+
+    def execute(self, userdata):
+        self.mvbs = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+
+        self.goal_sub = rospy.Subscriber("move_base_simple/goal", PoseStamped,
+                                         self._update_waypoints) #This callback should connect to camera and take a picture. Switch states here.
+
+        self.amcl_sub = rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped,
+                                         self._nearest_waypoint)
+
+        self.viz_pub = rospy.Publisher("patrolling/viz_waypoints_array",
+                                       MarkerArray, queue_size=10)
+        self._publish_markers()
+
+        self.mvbs.wait_for_server()
+        forward = True
+
+        rospy.wait_for_message("amcl_pose", PoseWithCovarianceStamped)
+
+        rospy.loginfo("Nearest waypoint is #{}".format(self.waypoint_index))
+
+        rospy.loginfo(self.waypoint_index)
+
+        rospy.loginfo(self.waypoints[self.waypoint_index])
+
+        self._publish_markers()
+
+        self.mvbs.send_goal(self.waypoints[self.waypoint_index])
+        self.mvbs.wait_for_result()
+        rospy.loginfo("Nav goal met, setting another one...")
+
+        # if self.waypoint_index >= len(self.waypoints) - 1:
+        #     forward = False
+        # elif self.waypoint_index <= 0:
+        #     forward = True
+        #
+        # if forward:
+        #     self.waypoint_index = self.waypoint_index + 1
+        # else:
+        #     self.waypoint_index = self.waypoint_index - 1
+        return 'click_picture'
+
+class click_picture(smach.State):
     def __init__(self):
         smach.State.__init__(self,outcomes=[['num_eggs'])
         global radius
@@ -246,140 +309,31 @@ def click_picture(smach.State):
 
         return 'num_eggs'
 
-
-#Navigate Waypoints State
-class prowl(smach.State):
+class terminate(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['prowl_pass', 'prowl_fail'])
-        self.waypoints = []
-        self.waypoint_index = None
-
-        rospy.init_node('waypoint_nav')
-        waypoint_arr = rospy.get_param('/navigation/waypoints')
-        rospy.loginfo('Printing arr:: %s', waypoint_arr)
-        rospy.loginfo('Length of array:: %2f',len(waypoint_arr)-1 )
-        for wp in waypoint_arr:
-            temp = MoveBaseGoal()
-
-            rospy.loginfo('wp[0]::%2f', wp[0])
-            rospy.loginfo('wp[1]::%2f', wp[1])
-            rospy.loginfo('wp[2]::%2f', wp[2])
-            rospy.loginfo('wp[3]::%2f', wp[3])
-
-            temp.target_pose.header.frame_id = 'map'
-            temp.target_pose.pose.position.x = wp[0]
-            temp.target_pose.pose.position.y = wp[1]
-            temp.target_pose.pose.orientation.z = wp[2]
-            temp.target_pose.pose.orientation.w = wp[3]
-
-            self.waypoints.append(temp)
-
-
-        rospy.loginfo("Waypoint Nav ready.")
-
-    def _nearest_waypoint(self, pose):
-        if not self.waypoint_index:
-            shortest_len = float('inf')
-            shortest_len_index = 0
-            x = pose.pose.pose.position.x
-            y = pose.pose.pose.position.y
-
-            for i, wp in enumerate(self.waypoints):
-                test_len = math.hypot(wp.target_pose.pose.position.x - x,
-                                      wp.target_pose.pose.position.y - y)
-                if test_len < shortest_len:
-                    shortest_len = test_len
-                    shortest_len_index = i
-
-            self.waypoint_index = shortest_len_index
-
-    def _update_waypoints(self, data):
-        latest = MoveBaseGoal(target_pose = data)
-
-        if rospy.get_param('/waypoints_nav/patrolling/update_patrol'):
-            self.waypoints.insert(self.waypoint_index, latest)
-
-            if rospy.get_param('/waypoints_nav/patrolling/save_latest'):
-                ros.set_param_raw('/waypoints_nav/patrolling/waypoints',
-                                  self.waypoints)
-
-    def _publish_markers(self):
-        markers = []
-        for i, wp in enumerate(self.waypoints):
-            temp = Marker()
-
-            temp.header = wp.target_pose.header
-            temp.id = i
-            temp.ns = "patrolling"
-            temp.action = Marker.ADD
-
-            temp.type = Marker.ARROW
-            temp.pose = wp.target_pose.pose
-            temp.scale.x = 0.6
-            temp.scale.y = 0.3
-            temp.scale.z = 0.3
-            temp.color.a = 1
-            temp.color.r = 0
-            temp.color.g = 0.5
-            temp.color.b = 0.5
-            markers.append(temp)
-        self.viz_pub.publish(markers=markers)
-
-        rospy.loginfo("Markers Published")
+        smach.State.__init__(self,outcomes=['terminate_success'])
 
     def execute(self, userdata):
-        self.mvbs = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-
-        self.goal_sub = rospy.Subscriber("move_base_simple/goal", PoseStamped,
-                                         self._update_waypoints) #This callback should connect to camera and take a picture. Switch states here.
-
-        self.amcl_sub = rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped,
-                                         self._nearest_waypoint)
-
-        self.viz_pub = rospy.Publisher("patrolling/viz_waypoints_array",
-                                       MarkerArray, queue_size=10)
-        self._publish_markers()
-
-        self.mvbs.wait_for_server()
-        forward = True
-
-        rospy.wait_for_message("amcl_pose", PoseWithCovarianceStamped)
-
-        rospy.loginfo("Nearest waypoint is #{}".format(self.waypoint_index))
-
-        rospy.loginfo(self.waypoint_index)
-
-        rospy.loginfo(self.waypoints[self.waypoint_index])
-
-        self._publish_markers()
-
-        self.mvbs.send_goal(self.waypoints[self.waypoint_index])
-        self.mvbs.wait_for_result()
-        rospy.loginfo("Nav goal met, setting another one...")
-
-        # if self.waypoint_index >= len(self.waypoints) - 1:
-        #     forward = False
-        # elif self.waypoint_index <= 0:
-        #     forward = True
-        #
-        # if forward:
-        #     self.waypoint_index = self.waypoint_index + 1
-        # else:
-        #     self.waypoint_index = self.waypoint_index - 1
-        return 'prowl_pass'
-
-# Error
-# class handle_error_state(smach.State):
-#     def __init__(self):
-#         smach.State.__init__(self, outcomes=['error_handling_done'])
-#
-#     def execute(self, userdata):
-#         while not(rospy.is_shutdown()):
-#             pass
+	global waypoints
+        #rospy.loginfo('In terminate smach state')
+        #package ='map_server'
+        #executable ='map_saver'
+        #node = roslaunch.core.Node(package, executable, args="-f "+str(os.path.dirname(os.path.realpath(__file__)))+"/maps/map")
+        #launch = roslaunch.scriptapi.ROSLaunch()
+        #launch.start()
+        #process = launch.launch(node)
+        #while process.is_alive():
+        #   pass
+        #rospy.loginfo('In Terminate Success')
+        #waypoints = rospy.get_param('/navigation/waypoints')
+        #rospy.loginfo('Waypoints: %s', waypoints)
+        #rospy.loginfo('Dumping waypoints')
+        #os.system("rosparam dump "+str(os.path.dirname(os.path.realpath(__file__)))+"/waypoints/waypoints.yaml /navigation/waypoints")
+        return 'terminate_success'
 
 
 def main():
-    rospy.init_node("egg_hunter")
+    rospy.init_node("egg_hunting")
 
     sm = smach.StateMachine(outcomes=['waypoint_nav_sm_init'])
 
@@ -387,8 +341,13 @@ def main():
     with sm:
         # Adding states to the container
         smach.StateMachine.add('prowl', prowl(),
-            transitions={'prowl_pass':'click_picture',
-                         'prowl_fail':'handle_error_state'})
+            transitions={'prowl_pass':'click_picture'})
+
+        smach.StateMachine.add('click_picture', click_picture(),
+transitions={'click_picture_success':'prowl'})
+    
+        smach.StateMachine.add('terminate', terminate(),
+transitions={'terminate_success':'waypoint_nav_sm_init'})
 
         # smach.StateMachine.add('handle_error_state', handle_error_state(),
         #     transitions={'error_handling_done':'handle_error_state'})
