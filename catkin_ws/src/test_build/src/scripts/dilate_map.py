3f7d5b1d6fc82faf9ce2ab80f5e7e8281bd722d2
Prithvi Kambhampati <pkambham@mtu.edu>
2017-04-26 23:26:59 -0400

    images

---
+++ catkin_ws/src/test_build/src/scripts/dilate_map.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+cv2.namedWindow("window1", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("window1", 640,480)
+img = cv2.imread('maps/map.pgm',0)
+img = 255-img
+kernel = np.ones((5,5),np.uint8)
+dilated_map = cv2.dilate(img,kernel,iterations = 3)
+dilated_map = 255-dilated_map
+cv2.imwrite('maps/dilated_map.pgm',dilated_map)
+cv2.imshow("window1", dilated_map)
+cv2.waitKey(0)
